dbed0f0d6aaeb1a1133755a32809bbbde6d66be2
Andrei Antonov <polymorphm@gmail.com>
2019-04-01 00:55:09 +0300

    using of variables $new_bin_path, $old_bin_path_startswith, $data_parent_dir

--- archlinux-postgresql-upgrade.sh
+++ archlinux-postgresql-upgrade.sh
@@ -3,6 +3,9 @@
 set -e
 
 utility_name='archlinux-postgresql-upgrade'
+new_bin_path='/usr/bin'
+old_bin_path_startswith='/opt/pgsql'
+data_parent_dir='/var/lib/postgres'
 os_user='postgres'
 
 if [ "x$#" != "x0" ]
@@ -24,85 +27,85 @@ quote ()
 
 echo "$utility_name: removing pre-old data files..."
 
-rm -rf -- /var/lib/postgres/old-data.removing
+rm -rf -- "$data_parent_dir/old-data.removing"
 
-if [ -d /var/lib/postgres/old-data ] &&
-        [ -f /var/lib/postgres/old-data/PG_VERSION ] &&
-        [ ! -x "/opt/pgsql-$(cat -- /var/lib/postgres/old-data/PG_VERSION)/bin/pg_upgrade" ]
+if [ -d "$data_parent_dir/old-data" ] &&
+        [ -f "$data_parent_dir/old-data/PG_VERSION" ] &&
+        [ ! -x "$old_bin_path_startswith-$(cat -- "$data_parent_dir/old-data/PG_VERSION")/bin/pg_upgrade" ]
 then
-    mv -- /var/lib/postgres/old-data /var/lib/postgres/old-data.removing
+    mv -- "$data_parent_dir/old-data" "$data_parent_dir/old-data.removing"
 fi
 
-rm -rf -- /var/lib/postgres/old-data.removing
+rm -rf -- "$data_parent_dir/old-data.removing"
 
 echo "$utility_name: removing pre-old data files: DONE!"
 
 echo "$utility_name: preparing old data files..."
 
-if [ ! -d /var/lib/postgres/old-data ] &&
-        [ -d /var/lib/postgres/data ] &&
-        [ -f /var/lib/postgres/data/PG_VERSION ] &&
-        [ -x "/opt/pgsql-$(cat -- /var/lib/postgres/data/PG_VERSION)/bin/pg_upgrade" ]
+if [ ! -d "$data_parent_dir/old-data" ] &&
+        [ -d "$data_parent_dir/data" ] &&
+        [ -f "$data_parent_dir/data/PG_VERSION" ] &&
+        [ -x "$old_bin_path_startswith-$(cat -- "$data_parent_dir/data/PG_VERSION")/bin/pg_upgrade" ]
 then
-    mv -- /var/lib/postgres/data /var/lib/postgres/old-data
+    mv -- "$data_parent_dir/data" "$data_parent_dir/old-data"
 fi
 
-if [ ! -d /var/lib/postgres/old-data ]
+if [ ! -d "$data_parent_dir/old-data" ]
 then
     echo "$utility_name: error: unable to find old data files" 1>&2
     exit 1
 fi
 
-if [ ! -f /var/lib/postgres/old-data/pg_ident.conf.saved ]
+if [ ! -f "$data_parent_dir/old-data/pg_ident.conf.saved" ]
 then
-    mv -- /var/lib/postgres/old-data/pg_ident.conf /var/lib/postgres/old-data/pg_ident.conf.saved
+    mv -- "$data_parent_dir/old-data/pg_ident.conf" "$data_parent_dir/old-data/pg_ident.conf.saved"
 fi
 
-if [ ! -f /var/lib/postgres/old-data/pg_hba.conf.saved ]
+if [ ! -f "$data_parent_dir/old-data/pg_hba.conf.saved" ]
 then
-    mv -- /var/lib/postgres/old-data/pg_hba.conf /var/lib/postgres/old-data/pg_hba.conf.saved
+    mv -- "$data_parent_dir/old-data/pg_hba.conf" "$data_parent_dir/old-data/pg_hba.conf.saved"
 fi
 
-if [ ! -f /var/lib/postgres/old-data/pg_hba.conf ]
+if [ ! -f "$data_parent_dir/old-data/pg_hba.conf" ]
 then
-    rm -f -- /var/lib/postgres/old-data/pg_hba.conf.new
-    (umask 0077 && touch /var/lib/postgres/old-data/pg_hba.conf.new)
-    echo 'local all all trust' >/var/lib/postgres/old-data/pg_hba.conf.new
-    chown -- "$os_user:$os_user" /var/lib/postgres/old-data/pg_hba.conf.new
-    mv -- /var/lib/postgres/old-data/pg_hba.conf.new /var/lib/postgres/old-data/pg_hba.conf
+    rm -f -- "$data_parent_dir/old-data/pg_hba.conf.new"
+    (umask 0077 && touch -- "$data_parent_dir/old-data/pg_hba.conf.new")
+    echo 'local all all trust' >"$data_parent_dir/old-data/pg_hba.conf.new"
+    chown -- "$os_user:$os_user" "$data_parent_dir/old-data/pg_hba.conf.new"
+    mv -- "$data_parent_dir/old-data/pg_hba.conf.new" "$data_parent_dir/old-data/pg_hba.conf"
 fi
 
 echo "$utility_name: preparing old data files: DONE!"
 
 echo "$utility_name: preparing new data files..."
 
-if [ -d /var/lib/postgres/data ]
+if [ -d "$data_parent_dir/data" ]
 then
     echo "$utility_name: error: new data files already exist" 1>&2
     exit 1
 fi
 
-rm -rf -- /var/lib/postgres/new-data
-mkdir -m0700 -- /var/lib/postgres/new-data
-chown -- "$os_user:$os_user" /var/lib/postgres/new-data
+rm -rf -- "$data_parent_dir/new-data"
+mkdir -m0700 -- "$data_parent_dir/new-data"
+chown -- "$os_user:$os_user" "$data_parent_dir/new-data"
 
-su -lc'/usr/bin/initdb -D/var/lib/postgres/new-data' -- "$os_user"
+su -lc"$(quote "$new_bin_path/initdb") -D$(quote "$data_parent_dir/new-data")" -- "$os_user"
 
-cp -p -- /var/lib/postgres/old-data/postgresql.auto.conf /var/lib/postgres/new-data/postgresql.auto.conf
-cp -p -- /var/lib/postgres/old-data/pg_hba.conf.saved /var/lib/postgres/new-data/pg_hba.conf.saved
-cp -p -- /var/lib/postgres/old-data/pg_ident.conf.saved /var/lib/postgres/new-data/pg_ident.conf.saved
+cp -p -- "$data_parent_dir/old-data/postgresql.auto.conf" "$data_parent_dir/new-data/postgresql.auto.conf"
+cp -p -- "$data_parent_dir/old-data/pg_hba.conf.saved" "$data_parent_dir/new-data/pg_hba.conf.saved"
+cp -p -- "$data_parent_dir/old-data/pg_ident.conf.saved" "$data_parent_dir/new-data/pg_ident.conf.saved"
 
 echo "$utility_name: preparing new data files: DONE!"
 
 echo "$utility_name: data migration..."
 
-su -lc"cd -- /var/lib/postgres/new-data && /usr/bin/pg_upgrade -b"$(quote "/opt/pgsql-$(cat -- /var/lib/postgres/old-data/PG_VERSION)/bin")" -B/usr/bin -d/var/lib/postgres/old-data -D/var/lib/postgres/new-data" -- "$os_user"
+su -lc"cd -- $(quote "$data_parent_dir/new-data") && $(quote "$new_bin_path/pg_upgrade") -b"$(quote "$old_bin_path_startswith-$(cat -- "$data_parent_dir/old-data/PG_VERSION")/bin")" -B$(quote "$new_bin_path") -d$(quote "$data_parent_dir/old-data") -D$(quote "$data_parent_dir/new-data")" -- "$os_user"
 
-mv -- /var/lib/postgres/new-data/pg_hba.conf /var/lib/postgres/new-data/pg_hba.conf.original
-mv -- /var/lib/postgres/new-data/pg_hba.conf.saved /var/lib/postgres/new-data/pg_hba.conf
-mv -- /var/lib/postgres/new-data/pg_ident.conf /var/lib/postgres/new-data/pg_ident.conf.original
-mv -- /var/lib/postgres/new-data/pg_ident.conf.saved /var/lib/postgres/new-data/pg_ident.conf
-mv -- /var/lib/postgres/new-data /var/lib/postgres/data
+mv -- "$data_parent_dir/new-data/pg_hba.conf" "$data_parent_dir/new-data/pg_hba.conf.original"
+mv -- "$data_parent_dir/new-data/pg_hba.conf.saved" "$data_parent_dir/new-data/pg_hba.conf"
+mv -- "$data_parent_dir/new-data/pg_ident.conf" "$data_parent_dir/new-data/pg_ident.conf.original"
+mv -- "$data_parent_dir/new-data/pg_ident.conf.saved" "$data_parent_dir/new-data/pg_ident.conf"
+mv -- "$data_parent_dir/new-data" "$data_parent_dir/data"
 
 echo "$utility_name: data migration: DONE!"
 
